f91d8e41b7fd99b488c42d7ec4ef13179ba4db9c
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2023-01-31 02:52:18 +0300

    Initialized commit

---
+++ src/Java/Welcome_To_Java.java
@@ -0,0 +1,12 @@
+package Java;
+
+public class Welcome_To_Java {
+    public class Solution {
+
+        public static void main(String[] args) {
+            System.out.println("Hello, World.");
+            System.out.println("Hello, Java.");
+        }
+    }
+
+}
