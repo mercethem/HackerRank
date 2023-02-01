f4da26bb5cd4286e5822c001dcb7013102c0e442
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2023-02-02 01:00:09 +0300

    Initialize commit

---
+++ src/Java/Stdin_And_Stdout_II.java
@@ -0,0 +1,26 @@
+package Java;
+
+import java.util.*;
+
+public class Stdin_And_Stdout_II {
+
+
+    public class Solution {
+
+        public static void main(String[] args) {
+            Scanner keyboard = new Scanner(System.in);
+            int i = keyboard.nextInt();
+            double d = keyboard.nextDouble();
+            String s = keyboard.nextLine();
+            s = keyboard.nextLine();
+
+
+
+
+            System.out.println("String: " + s);
+            System.out.println("Double: " + d);
+            System.out.println("Int: " + i);
+        }
+    }
+
+}
