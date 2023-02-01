3ec9bdc7111bb82a0471a6a71b4151a7295edb83
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2023-02-02 01:00:05 +0300

    Initialize commit

---
+++ src/Java/Stdin_And_Stdout_I.java
@@ -0,0 +1,22 @@
+package Java;
+
+import java.util.*;
+
+public class Stdin_And_Stdout_I {
+
+
+    public class Solution {
+
+        public static void main(String[] args) {
+            Scanner scan = new Scanner(System.in);
+            int a = scan.nextInt();
+            int b = scan.nextInt();
+            int c = scan.nextInt();
+
+            System.out.println(a);
+            System.out.println(b);
+            System.out.println(c);
+        }
+    }
+
+}
