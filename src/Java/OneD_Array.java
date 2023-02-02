13c1a5cbf51abc3472790daa7c247021d7a88b73
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2023-02-03 00:26:48 +0300

    Initialize commit

---
+++ src/Java/OneD_Array.java
@@ -0,0 +1,9 @@
+package Java;
+
+public class OneD_Array {
+//    int a[] = new int[n];
+    int counter = 0;
+//        while(scan.hasNextInt()){
+//        a[counter] = scan.nextInt();
+//        counter++;
+    }
