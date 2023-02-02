b665ea8bd26a807f3dff828b4aca9c3a0710d9fe
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2023-02-03 00:26:40 +0300

    Initialize commit

---
+++ src/Java/Inheritance.java
@@ -0,0 +1,11 @@
+package Java;
+
+public class Inheritance {
+//    class Bird extends Animal{
+        void fly(){
+            System.out.println("I am flying");
+        }
+        void sing(){
+            System.out.println("I am singing");
+        }
+}
