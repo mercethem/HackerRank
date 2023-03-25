e218592970d2f0c6ab93c149afce3f5fa29af59b
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2023-03-26 00:36:36 +0300

    Initial commit

---
+++ src/C/Sum_and_Difference_of_Two_Numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+	int x, y;
+    float a, b;
+    scanf("%d%d",&x,&y);
+    scanf("%f%f",&a,&b);
+    printf("%d %d\n",x + y, x - y);
+    printf("%.1f %.1f\n", a + b, a - b);
+    return 0;
+}
