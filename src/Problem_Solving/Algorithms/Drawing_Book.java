d300be8e571762c9627b1d68f4d30657c5d2967a
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2023-09-18 15:54:16 +0300

    initial commit

---
+++ src/Problem_Solving/Algorithms/Drawing_Book.java
@@ -0,0 +1,60 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'pageCount' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER p
+     */
+
+    public static int pageCount(int n, int p) {
+        // Write your code here
+        //n is how much page in book
+        //p is how much turn page
+
+        //even numbers left page
+        //odd numbers rightpage
+
+        int countStoF = 0; //from Start to Finish
+        int countFtoS = 0; //from Finish to Start
+
+        countStoF = p/2;
+        countFtoS = n/2 - countStoF;
+
+        return Math.min(countStoF, countFtoS);
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        int p = Integer.parseInt(bufferedReader.readLine().trim());
+
+        int result = Result.pageCount(n, p);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
